ef4897c92881e49f8aa2858b1e6fe315dbede32f
Rituraj Mishra <riturajmishra719@gmail.com>
2023-03-01 01:34:29 +0530

    Initial Commit

---
+++ Flashgram/FlashgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashgramApp.swift
+//  Flashgram
+//
+//  Created by Rituraj Mishra on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
